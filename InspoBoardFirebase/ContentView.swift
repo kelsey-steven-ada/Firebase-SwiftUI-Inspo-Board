65bed9cfd0c9c764a35c3c6f07002f9ceeb029ce
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2022-08-10 16:17:58 -0700

    Initial Commit

---
+++ InspoBoardFirebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InspoBoardFirebase
+//
+//  Created by Kelsey Steven on 8/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InspoBoardFirebase/InspoBoardFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspoBoardFirebaseApp.swift
+//  InspoBoardFirebase
+//
+//  Created by Kelsey Steven on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct InspoBoardFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
